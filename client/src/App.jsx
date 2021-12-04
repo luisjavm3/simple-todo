c83bc6962224cf62440ace7dcf20fb45880f3d26
Luis J. Montes <luisjavm3@gmail.com>
2021-12-04 10:05:38 -0500

    Remove data from localStorage when application closes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,10 @@ import AppRoutes from './router/AppRoutes';
 import { AuthProvider } from './utils/useAuth';
 
 function App() {
+  window.onbeforeunload = () => {
+    localStorage.removeItem('simple_todo');
+  };
+
   return (
     <AuthProvider>
       <AppRoutes />
